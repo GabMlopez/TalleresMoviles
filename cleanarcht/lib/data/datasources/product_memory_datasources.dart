122d6e0ea812a2c4d09c6a8f368aaea00f4e2494
GabrielLopez <gmlopez11@espe.edu.ec>
2025-12-02 10:28:07 -0500

    datasource product

---
+++ cleanarcht/lib/data/datasources/product_memory_datasources.dart
@@ -0,0 +1,26 @@
+import '../../domain/entities/products.dart';
+
+class ProductMemoryDatasources{
+  final List <Product> _products = [];
+
+  //Cargar la lista de datos
+  List<Product> getAll()=> _products;
+
+  //Agregar el objeto
+  void add(Product product) => _products.add(product);
+
+  //Actualizar producto
+  void update(Product product) {
+    final index = _products.indexWhere(
+        (e)=>e.id == product.id
+    );
+    if(index !=-1) _products[index]= product;
+  }
+
+  //Eliminar producto
+  void delete(Product product){
+    _products.removeWhere(
+            (e)=>e.id == product.id
+    );
+  }
+}
--- cleanarcht/lib/domain/usescase/delete_product.dart
+++ cleanarcht/lib/domain/usescase/delete_product.dart
@@ -1,4 +1,4 @@
-import '../repositories/product_repository.dart';
+import '../repository/product_repository.dart';
 
 class DeleteProduct{
   final ProductRepository repository;
