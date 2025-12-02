dffcbeb299a4f9fb1cd3d7fca037ae4f552b2b02
MATEOCAIZA <caizamateo86@gmail.com>
2025-12-02 10:55:41 -0500

    Providers

---
+++ cleanarcht/lib/presentation/providers/product_provider.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/cupertino.dart';
+
+import '../../domain/entities/product.dart';
+import '../../domain/usescase/add_product.dart';
+import '../../domain/usescase/delete_product.dart';
+import '../../domain/usescase/get_products.dart';
+import '../../domain/usescase/update_product.dart';
+
+class ProductProvider extends ChangeNotifier{
+  final GetProducts getProductsUseCase;
+  final AddProduct addProductUseCase;
+  final UpdateProduct updateProductUseCase;
+  final DeleteProduct deleteProductUseCase;
+
+  ProductProvider({
+    required this.getProductsUseCase,
+    required this.addProductUseCase,
+    required this.updateProductUseCase,
+    required this.deleteProductUseCase,
+  });
+
+  List get products => getProductsUseCase();
+
+  void add(Product product){
+    addProductUseCase(product);
+    notifyListeners();
+  }
+
+  void update(Product product){
+    updateProductUseCase(product);
+    notifyListeners();
+  }
+
+  void delete(String id){
+    deleteProductUseCase(id);
+    notifyListeners();
+  }
+
+}
