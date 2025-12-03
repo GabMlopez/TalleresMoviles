e8d10a66304f9e805956b7ddd651efbe52071ba7
GabrielLopez <gmlopez11@espe.edu.ec>
2025-12-03 15:02:28 -0500

    product añadir

---
+++ cleanarcht/lib/presentation/layouts/product_detail.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../../domain/entities/products.dart';
+import '../providers/product_provider.dart';
+
+class ProductDetailPage extends StatefulWidget {
+  const ProductDetailPage({super.key});
+
+  @override
+  State<ProductDetailPage> createState() => _ProductDetailPageState();
+}
+
+class _ProductDetailPageState extends State<ProductDetailPage> {
+  final nameController = TextEditingController();
+  final priceController = TextEditingController();
+  late String? productId;
+  //Sobrescribir deoendencias
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    final args = ModalRoute.of(context)?.settings.arguments as Product;
+    if(args != null){
+      productId = args.id;
+      nameController.text=args.name;
+      priceController.text=args.price.toString();
+    }
+  }
+  @override
+  Widget build(BuildContext context) {
+    final provider=Provider.of<ProductProvider>(context);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Detalle de Prducto"),
+      ),
+      body: Column(
+        children: [
+          TextField(
+            controller: nameController,
+            decoration: const InputDecoration(
+              labelText: "Nombre producto"
+            ),
+          ),
+          const SizedBox(height:20,),
+          TextField(
+            controller: priceController,
+            decoration: const InputDecoration(
+                labelText: "Precio producto"
+            )
+          ),
+          const SizedBox(height:20,),
+          ElevatedButton(onPressed: (){
+            if(productId !=null){
+              provider.add(
+                Product(DateTime.now().microsecondsSinceEpoch.toString(),
+                    nameController.text,
+                    double.parse(priceController.text))
+              );
+            }
+          }, child: Text("Registrar"))
+        ],
+      ),
+    );
+  }
+}
